e80d529b3abb96567f0d18b9dd987dfd08f7d625
aurélien <mitchi09099@gmail.com>
2019-03-28 17:23:23 +0100

    texte acceuil

--- javascript.js
+++ javascript.js
@@ -1,7 +1,11 @@
-var btn = document.querySelector('.toggle_btn');
-var nav = document.querySelector('.nav');
-
-btn.onclick = function(){
-    nav.classList.toggle('nav_open');
-}
-
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "white";
+  }
